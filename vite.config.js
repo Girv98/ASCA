5b518122ed6365866b35df037a14f5b1f5e5855b
Girv98 <james.girven10@gmail.com>
2026-05-13 03:13:44 +0100

    update deps

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   optimizeDeps: {
-    esbuildOptions: {
-      target: 'esnext'
+    rolldownOptions: {
+      "target": 'esnext'
     }
   },
   build: {
